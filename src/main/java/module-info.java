20fe738affac15aa16781932aa3c8438c15e9058
jcarr <j5carroll@student.bridgew.edu>
2021-11-09 10:24:48 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.second_webdata {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires javafx.graphics;
+
+    opens com.example.second_webdata to javafx.fxml;
+    exports com.example.second_webdata;
+}
